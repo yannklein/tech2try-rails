6d16eba0d90663d619dbe797f172cd7aeb230772
Yann Klein <yann.klein@me.com>
2019-11-19 15:09:19 +0900

    flatpickr imporvement

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -1,9 +1,13 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {})
+  flatpickr("#product-form-start", {
+    altInput: true,
+    plugins: [new rangePlugin({ input: "#product-form-end"})]
+  })
 
   document.querySelector("#product-form-start").addEventListener("change", (event) => {
     window.location.href = '#product-form-end';
