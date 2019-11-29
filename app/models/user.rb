42797079ab826dde766c8715f4913df71e7a17b1
Yann Klein <yann.klein@me.com>
2019-11-29 13:04:58 +0900

    chat placeholders

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :techgets
-  has_many :rentals
+  # has_many :rentals, foreign_key: :renter_id
   has_many :messages
-  belongs_to :chat_room
+  belongs_to :chat_room, required: false
   mount_uploader :photo, PhotoUploader
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def rentals
+    Rental.where("renter_id = ? OR rentee_id= ?", self.id, self.id)
+  end
 end
